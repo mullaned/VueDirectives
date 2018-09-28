1caa1a80ecb56c410ff391ba68258c99df0a355b
David M <mullaned@gmail.com>
2018-09-28 18:06:31 +0100

    set directive option from the bind

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
-    el.style.backgroundColor = 'Green';
+    // el.style.backgroundColor = 'Green';
+    el.style.backgroundColor = binding.value;
   }  
 });
 
