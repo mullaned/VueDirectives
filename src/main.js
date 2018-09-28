d69a8501a730f15712723f043c937aa0b414982d
David M <mullaned@gmail.com>
2018-09-28 18:13:42 +0100

    directive modifiers

--- src/main.js
+++ src/main.js
@@ -5,12 +5,20 @@ Vue.directive('highlight', {
   bind(el, binding, vnode) {
     // el.style.backgroundColor = 'Green';
     // el.style.backgroundColor = binding.value;
+    let delay = 0;
+    if(binding.modifiers['delayed']){
+        delay=3000;
 
-    if(binding.arg== 'background') {
-      el.style.backgroundColor = binding.value;
-    }else {
-      el.style.color = binding.value;
     }
+    setTimeout(() => {
+      if(binding.arg== 'background') {
+        el.style.backgroundColor = binding.value;
+      }else {
+        el.style.color = binding.value;
+      }
+    },delay);
+
+    
   }  
 });
 
