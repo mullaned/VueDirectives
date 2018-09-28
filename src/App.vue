8f3fa6253a9f5fb5277d752682f7d99262bb6aad
David M <mullaned@gmail.com>
2018-09-28 18:31:31 +0100

    local directive modifier

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color This</p>
-                <p v-local-highlight:background.delayed="'red'">Color This aswell</p>
+                <p v-local-highlight:background.delayed.blink="'red'">Color This aswell</p>
             </div>
         </div>
     </div>
@@ -28,13 +28,31 @@
                         delay=3000;
 
                     }
-                    setTimeout(() => {
-                    if(binding.arg== 'background') {
-                        el.style.backgroundColor = binding.value;
+
+                    if(binding.modifiers['blink']){
+                        let mainColor = binding.value;
+                        let secondColor = 'Blue';
+                        let currentColor = mainColor;
+
+                        setTimeout(() => {
+                            setInterval(() => {
+                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+                                    if(binding.arg== 'background') {
+                                    el.style.backgroundColor = currentColor;
+                                }else {
+                                    el.style.color = currentColor;
+                                }
+                                },1000);
+                            },delay);
                     }else {
-                        el.style.color = binding.value;
-                    }
-                    },delay);
+                        setTimeout(() => {
+                        if(binding.arg== 'background') {
+                            el.style.backgroundColor = binding.value;
+                        }else {
+                            el.style.color = binding.value;
+                        }
+                        },delay);
+                    }                
                 }
             }
         }
